79a58e7e67cc5e63f20257ee770372f656664ae8
Uhetech <ibiamfaith88@gmail.com>
2026-02-02 20:08:29 +0100

    Rename IAM role and policy attachment resources

--- https:/iam.tf
+++ https:/iam.tf
@@ -1,19 +1,17 @@
 resource "aws_iam_role" "ec2_ssm_role" {
-  name = "ec2-ssm-role-2"
+  name = "ec2-ssm-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
     }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ssm_core" {
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
   role       = aws_iam_role.ec2_ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
