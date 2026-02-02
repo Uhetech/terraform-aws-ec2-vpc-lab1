9adeda8d8a1bfb9bb3e59397dedc0c17e07166a2
Uhetech <ibiamfaith88@gmail.com>
2026-02-02 18:34:20 +0100

    Create security-group.tf

---
+++ https:/https:/https:/https:/security-group.tf
@@ -0,0 +1,15 @@
+resource "aws_security_group" "ec2_sg" {
+  name   = "ec2-sg"
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-sg"
+  }
+}
