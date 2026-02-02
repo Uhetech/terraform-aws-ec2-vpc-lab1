3cc84bdeb5c04fbb11976ac298e911bff2f38dc4
Uhetech <ibiamfaith88@gmail.com>
2026-02-02 19:26:36 +0100

    Refactor EC2 configuration by removing security group
    Removed security group resource and updated instance profile and tags.

--- https:/https:/ec2.tf
+++ https:/https:/ec2.tf
@@ -1,31 +1,11 @@
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2-sg"
-  description = "Allow SSH"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_instance" "ec2" {
-  ami                    = "ami-0fc5d935ebf8bc3bc" # Amazon Linux 2 us-east-1
+  ami                    = "ami-0fc5d935ebf8bc3bc"
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
 
   tags = {
-    Name = "terraform-ec2"
+    Name = "terraform-ec2-ssm"
   }
 }
