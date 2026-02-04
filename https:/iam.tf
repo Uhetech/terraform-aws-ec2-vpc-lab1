02c4d1f957c575ae72f622bd648451e8efaa3b54
Uhetech <ibiamfaith88@gmail.com>
2026-02-04 11:45:15 +0100

    Refactor IAM roles and instance profiles for SSM

--- https:/iam.tf
+++ https:/iam.tf
@@ -1,22 +1,25 @@
-resource "aws_iam_role" "ec2_ssm_role" {
-  name = "ec2-ssm-role"
+resource "aws_iam_role" "ssm_role" {
+  name = "terraform-ec2-ssm-role-v2"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action    = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
     }]
   })
 }
 
+
 resource "aws_iam_role_policy_attachment" "ssm_attach" {
-  role       = aws_iam_role.ec2_ssm_role.name
+  role       = aws_iam_role.ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2-ssm-profile"
-  role = aws_iam_role.ec2_ssm_role.name
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "terraform-ec2-ssm-profile-v3"
+  role = aws_iam_role.ssm_role.name
 }
